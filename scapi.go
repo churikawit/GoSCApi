962e9b86862c606614f8569d971a02f8d065f4b6
Karn <churikawit@gmail.com>
2022-03-30 12:41:22 +0700

    go fmt

--- scapi.go
+++ scapi.go
@@ -1,42 +1,42 @@
 package scapi
 
 import (
+	"errors"
+	"fmt"
+	"strconv"
+	"strings"
 	"syscall"
 	"unicode/utf16"
 	"unsafe"
-	"strconv"
-	"errors"
-	"strings"
-	"fmt"
 )
 
 const (
 	SAS_INT_AUTH_RMACKEY_ADMIN = 0
-	SAS_INT_AUTH_FPKEY_ADMIN = 1
-	SAS_INT_AUTH_FPKEY_MOI = 2
-	SAS_INT_AUTH_FPKEY_HTH = 3
-	SAS_INT_AUTH_FPKEY_MOI2 = 4
-	SAS_INT_AUTH_FPKEY_MOI3 = 5
-	SAS_INT_AUTH_FPKEY_MOI4 = 6
-	SAS_INT_AUTH_FPKEY_WVOT = 7
-	SAS_INT_AUTH_FPKEY_MOD = 8
-	SAS_INT_AUTH_FPKEY_MOA = 9
-	SAS_INT_AUTH_FPKEY_NHSO = 10
+	SAS_INT_AUTH_FPKEY_ADMIN   = 1
+	SAS_INT_AUTH_FPKEY_MOI     = 2
+	SAS_INT_AUTH_FPKEY_HTH     = 3
+	SAS_INT_AUTH_FPKEY_MOI2    = 4
+	SAS_INT_AUTH_FPKEY_MOI3    = 5
+	SAS_INT_AUTH_FPKEY_MOI4    = 6
+	SAS_INT_AUTH_FPKEY_WVOT    = 7
+	SAS_INT_AUTH_FPKEY_MOD     = 8
+	SAS_INT_AUTH_FPKEY_MOA     = 9
+	SAS_INT_AUTH_FPKEY_NHSO    = 10
 )
 
 const (
-	CARD_UNKNOWN       = 20
-	CARD_JCOP_20       = 21
-	CARD_GXP_PROR3     = 22
-	CARD_ST_DP66_A     = 23
-	CARD_ST_DP66_B     = 24
-    CARD_JCOP_31       = 26
-    CARD_JCOP_31_27    = 27
-    CARD_KONA26CC      = 28
-    CARD_JCOP241R3     = 29    
-    CARD_JTOP20IDv2    = 30    
-    CARD_JTOP20IDv2L2  = 31
-	CARD_SAM_SRV       = 99
+	CARD_UNKNOWN      = 20
+	CARD_JCOP_20      = 21
+	CARD_GXP_PROR3    = 22
+	CARD_ST_DP66_A    = 23
+	CARD_ST_DP66_B    = 24
+	CARD_JCOP_31      = 26
+	CARD_JCOP_31_27   = 27
+	CARD_KONA26CC     = 28
+	CARD_JCOP241R3    = 29
+	CARD_JTOP20IDv2   = 30
+	CARD_JTOP20IDv2L2 = 31
+	CARD_SAM_SRV      = 99
 )
 
 // StringToCharPtr converts a Go string into pointer to a null-terminated cstring.
@@ -60,7 +60,7 @@ var (
 	procCloseReader    = scapiDLL.NewProc("CloseReader")
 	procGetCardStatus  = scapiDLL.NewProc("GetCardStatus")
 	procSelectApplet   = scapiDLL.NewProc("SelectApplet")
-	procReadData  	   = scapiDLL.NewProc("ReadData")
+	procReadData       = scapiDLL.NewProc("ReadData")
 	procGetCardInfo    = scapiDLL.NewProc("GetCardInfo")
 	procGetInfoADM     = scapiDLL.NewProc("GetInfoADM")
 	procVerifyPIN      = scapiDLL.NewProc("VerifyPIN")
@@ -115,10 +115,10 @@ func CloseReader() (err error) {
 
 func GetCardStatus() (err error) {
 	var status int32
-	atr := make([]byte, 1000);
-	var atr_len int = 100;
-	var timeOut int = 200;
-	var cardType int = 0;
+	atr := make([]byte, 1000)
+	var atr_len int = 100
+	var timeOut int = 200
+	var cardType int = 0
 	r, _, errno := procGetCardStatus.Call(uintptr(unsafe.Pointer(&atr[0])), uintptr(unsafe.Pointer(&atr_len)), uintptr(timeOut),
 		uintptr(unsafe.Pointer(&cardType)), uintptr(unsafe.Pointer(&status)))
 	_ = errno
@@ -131,16 +131,16 @@ func GetCardStatus() (err error) {
 	return
 }
 
-func GetCardInfo() (cid,chip,os,pre_perso,perso string ,err error) {
+func GetCardInfo() (cid, chip, os, pre_perso, perso string, err error) {
 	var status int32
-	b_cid := make([]byte, 20) 
+	b_cid := make([]byte, 20)
 	b_chip := make([]byte, 20)
 	b_os := make([]byte, 20)
 	b_pre_perso := make([]byte, 20)
 	b_perso := make([]byte, 20)
 
 	r, _, errno := procGetCardInfo.Call(
-		uintptr(unsafe.Pointer(&b_cid[0])), uintptr(unsafe.Pointer(&b_chip[0])), uintptr(unsafe.Pointer(&b_os[0])), 
+		uintptr(unsafe.Pointer(&b_cid[0])), uintptr(unsafe.Pointer(&b_chip[0])), uintptr(unsafe.Pointer(&b_os[0])),
 		uintptr(unsafe.Pointer(&b_pre_perso[0])), uintptr(unsafe.Pointer(&b_perso[0])), uintptr(unsafe.Pointer(&status)))
 	_ = errno
 	if r != 0 {
@@ -164,7 +164,7 @@ func GetInfoADM() (adm_version, laser_number string, err error) {
 	var authorize int32 = 0
 	b_laser_number := make([]byte, 33)
 
-	r, _, errno := procGetInfoADM.Call(uintptr(unsafe.Pointer(&b_version[0])), uintptr(unsafe.Pointer(&adm_status)) , 
+	r, _, errno := procGetInfoADM.Call(uintptr(unsafe.Pointer(&b_version[0])), uintptr(unsafe.Pointer(&adm_status)),
 		uintptr(unsafe.Pointer(&authorize)), uintptr(unsafe.Pointer(&b_laser_number[0])), uintptr(unsafe.Pointer(&status)))
 	_ = errno
 	if r != 0 {
@@ -194,7 +194,7 @@ func GetLaserNumber() (laser_number string, err error) {
 	if err != nil {
 		return
 	}
-	
+
 	adm_version, laser_number, err := GetInfoADM()
 	if err != nil {
 		return
@@ -212,7 +212,7 @@ func GetCID() (cid string, err error) {
 	if err != nil {
 		return
 	}
-	cid,_,_,_,_,err = GetCardInfo()
+	cid, _, _, _, _, err = GetCardInfo()
 	if err != nil {
 		cid = ""
 		return
@@ -223,20 +223,20 @@ func GetCID() (cid string, err error) {
 
 func Convert_LaserNumber(laser_number string) (output string) {
 	// "4d453131323039313334313900000000" -> "ME1120913419"
-	b := str2byte(laser_number);
+	b := str2byte(laser_number)
 	laser_number = decodeWindows874(string(b))
 	laser_number = strings.Trim(laser_number, "\000")
 	output = laser_number
 
-	return output;
+	return output
 }
 
 func SelectApplet(AppletID string) (err error) {
 	err = nil
 
 	var MOI_AID string = "A000000054480001"
-    var CM_AID string = "434D"
-    var ADM_AID string = "A000000084060002"
+	var CM_AID string = "434D"
+	var ADM_AID string = "A000000084060002"
 
 	if AppletID == "MOI_AID" {
 		AppletID = MOI_AID
@@ -249,7 +249,7 @@ func SelectApplet(AppletID string) (err error) {
 	var status int32
 	aid := str2byte(AppletID)
 	var aid_size int = len(aid)
-	r, _, errno := procSelectApplet.Call(uintptr(unsafe.Pointer(&aid[0])), uintptr(aid_size), uintptr(unsafe.Pointer(&status)) )
+	r, _, errno := procSelectApplet.Call(uintptr(unsafe.Pointer(&aid[0])), uintptr(aid_size), uintptr(unsafe.Pointer(&status)))
 	_ = errno
 	if r != 0 {
 		e := GetScapiStatus(status)
@@ -263,15 +263,15 @@ func SelectApplet(AppletID string) (err error) {
 
 func ReadData(block_id int, offset int, data_size int) (output []byte, err error) {
 	var status int32 = 0
-	if (data_size >= 0) {
-		output = make([]byte, data_size);
+	if data_size >= 0 {
+		output = make([]byte, data_size)
 	} else {
 		output = nil
 		err = nil
 		return nil, nil
 	}
 
-	ret, _, errno := procReadData.Call(uintptr(block_id), uintptr(offset), uintptr(data_size), uintptr(unsafe.Pointer(&output[0])), uintptr(unsafe.Pointer(&status)) );
+	ret, _, errno := procReadData.Call(uintptr(block_id), uintptr(offset), uintptr(data_size), uintptr(unsafe.Pointer(&output[0])), uintptr(unsafe.Pointer(&status)))
 	_ = errno
 	if ret != 0 {
 		e := GetScapiStatus(status)
@@ -287,7 +287,7 @@ func VerifyPIN() (tryremain int, err error) {
 	var pin_id int = 1
 	var share_data int = 0
 	var try_remain int = 0
-	ret, _, errno := procVerifyPIN.Call( uintptr(pin_id), uintptr(share_data), uintptr(unsafe.Pointer(&try_remain)), uintptr(unsafe.Pointer(&status)) )
+	ret, _, errno := procVerifyPIN.Call(uintptr(pin_id), uintptr(share_data), uintptr(unsafe.Pointer(&try_remain)), uintptr(unsafe.Pointer(&status)))
 	_ = errno
 	if ret != 0 {
 		e := GetScapiStatus(status)
@@ -299,7 +299,7 @@ func VerifyPIN() (tryremain int, err error) {
 	return
 }
 
-func GetMatchStatus( in_buf []byte) (out_buf []byte, match_stt int, err error) {
+func GetMatchStatus(in_buf []byte) (out_buf []byte, match_stt int, err error) {
 	var status int32 = 0
 	var req_type int = 1
 	var req_mode int = 0
@@ -313,8 +313,8 @@ func GetMatchStatus( in_buf []byte) (out_buf []byte, match_stt int, err error) {
 
 	in_size = len(in_buf)
 	out_buf = make([]byte, in_size)
-	ret, _, errno := procGetMatchStatus.Call(uintptr(req_type), uintptr(req_mode), uintptr(unsafe.Pointer(&in_buf[0])), uintptr(in_size), 
-		uintptr(unsafe.Pointer(&out_buf[0])),  uintptr(unsafe.Pointer(&out_size)) , uintptr(unsafe.Pointer(&match_stt)) , uintptr(unsafe.Pointer(&status)) )
+	ret, _, errno := procGetMatchStatus.Call(uintptr(req_type), uintptr(req_mode), uintptr(unsafe.Pointer(&in_buf[0])), uintptr(in_size),
+		uintptr(unsafe.Pointer(&out_buf[0])), uintptr(unsafe.Pointer(&out_size)), uintptr(unsafe.Pointer(&match_stt)), uintptr(unsafe.Pointer(&status)))
 
 	_ = errno
 	if ret != 0 {
@@ -326,7 +326,7 @@ func GetMatchStatus( in_buf []byte) (out_buf []byte, match_stt int, err error) {
 	}
 
 	return
-} 
+}
 
 func EnvelopeGMSx(in_buf []byte) (out_buf []byte, err error) {
 	var status int32 = 0
@@ -334,8 +334,8 @@ func EnvelopeGMSx(in_buf []byte) (out_buf []byte, err error) {
 	envelop := make([]byte, envelop_size)
 	in_size := len(in_buf)
 
-	ret, _, errno := procEnvelopeGMSx.Call( uintptr(SAS_INT_AUTH_FPKEY_ADMIN), uintptr(unsafe.Pointer(&in_buf[0])), uintptr(in_size), 
-		uintptr(unsafe.Pointer(&envelop[0])), uintptr(unsafe.Pointer(&envelop_size)), uintptr(unsafe.Pointer(&status)) )
+	ret, _, errno := procEnvelopeGMSx.Call(uintptr(SAS_INT_AUTH_FPKEY_ADMIN), uintptr(unsafe.Pointer(&in_buf[0])), uintptr(in_size),
+		uintptr(unsafe.Pointer(&envelop[0])), uintptr(unsafe.Pointer(&envelop_size)), uintptr(unsafe.Pointer(&status)))
 
 	_ = errno
 	if ret != 0 {
@@ -350,7 +350,7 @@ func EnvelopeGMSx(in_buf []byte) (out_buf []byte, err error) {
 }
 
 func ReadCardVersion() (string, error) {
-	data,err := ReadData(0, 0, 4)
+	data, err := ReadData(0, 0, 4)
 
 	if err != nil {
 		return "", err
@@ -360,9 +360,9 @@ func ReadCardVersion() (string, error) {
 
 func int2HexChar(d byte) byte {
 	// input 9 -> '9', 10 -> 'A'
-	if (d >= 0 && d <= 9) {
+	if d >= 0 && d <= 9 {
 		return (byte)('0' + d)
-	} else if (d >= 10 && d <= 15) {
+	} else if d >= 10 && d <= 15 {
 		return (byte)('A' + d - 10)
 	} else {
 		return (byte)('X')
@@ -384,12 +384,12 @@ func hexChar2Int(c byte) byte {
 
 func bin2Str(bin string) string {
 	// input: "5A" -> [0x35][0x41] -> "3541" -> output: [0x33][0x35][0x34][0x31] {double bytes}
-	var output string = "";
-	length := len(bin);
+	var output string = ""
+	length := len(bin)
 	for i := 0; i < length; i++ {
 		temp := int(bin[i])
 		high := byte(temp / 16)
-		low  := byte(temp % 16)
+		low := byte(temp % 16)
 
 		output += string(int2HexChar(high))
 		output += string(int2HexChar(low))
@@ -398,50 +398,50 @@ func bin2Str(bin string) string {
 }
 
 func str2byte(str string) []byte {
-	// input: "A000000054480001" 
+	// input: "A000000054480001"
 	// output: [0xA0][0x00][0x00][0x00][0x54][0x48][0x00][0x01] {reduce bytes}
 	b_str := []byte(str)
-	output_size := int((len(b_str)+1)/2)
+	output_size := int((len(b_str) + 1) / 2)
 	output := make([]byte, output_size)
 
 	i := 0
 	j := 0
 	var tmp int
-	for i = 0;i < len(b_str); i++ {
+	for i = 0; i < len(b_str); i++ {
 		tmp = int(hexChar2Int(b_str[i]))
 		tmp = tmp * 16
 		i++
-		if(str[i] == 0) {
+		if str[i] == 0 {
 			break
 		}
 		tmp = tmp + int(hexChar2Int(b_str[i]))
 		output[j] = (byte)(tmp)
 		j++
 	}
-	return output;
+	return output
 }
 
 func splitReader(buffer string) (reader_list []string) {
 	reader_list = make([]string, 0)
 
-	start_index := -1 
-	for i := 0 ; i < len(buffer); {
-		if (0x30 <= buffer[i] && buffer[i] <= 0x39) {
-			if (start_index == -1) {
+	start_index := -1
+	for i := 0; i < len(buffer); {
+		if 0x30 <= buffer[i] && buffer[i] <= 0x39 {
+			if start_index == -1 {
 				start_index = i
 			}
-			i++;
-		} else if (buffer[i] == 0) {
-			break;
+			i++
+		} else if buffer[i] == 0 {
+			break
 		} else {
-			if (start_index == -1) {
-				break;
+			if start_index == -1 {
+				break
 			}
 			len, err := strconv.Atoi(buffer[start_index:i])
 			if err != nil {
 				return
 			}
-			reader := buffer[i:i+len]
+			reader := buffer[i : i+len]
 			reader_list = append(reader_list, reader)
 
 			start_index = -1
@@ -458,18 +458,18 @@ func splitReader(buffer string) (reader_list []string) {
 
 func ReadCardData() (smartcard *SmartCard, err error) {
 	readerList := ListReader()
-	if (len(readerList) == 0) {
+	if len(readerList) == 0 {
 		e := fmt.Sprintf("[ListReader error: %v]", "no reader")
 		err = errors.New(e)
 		return
 	}
 
-	for i := 0 ;i < len(readerList); i++ {
+	for i := 0; i < len(readerList); i++ {
 		fmt.Printf("reader %d: %v\n", i, readerList[i])
 	}
 
 	// OpenReader
-	if (len(readerList) > 0) {
+	if len(readerList) > 0 {
 		err = OpenReader(readerList[0])
 		if err != nil {
 			e := fmt.Sprintf("[OpenReader error: %v]", err.Error())
@@ -496,57 +496,57 @@ func ReadCardData() (smartcard *SmartCard, err error) {
 
 	cardversion, err := ReadCardVersion()
 	if err != nil {
-		e:= fmt.Sprintf("[ReadCardVersion error: %v]", err.Error())
+		e := fmt.Sprintf("[ReadCardVersion error: %v]", err.Error())
 		err = errors.New(e)
 		return
 	}
 
 	// Read card data
 	if cardversion == "0003" {
-		data, err3_1 := ReadData(0, 0, 377);
+		data, err3_1 := ReadData(0, 0, 377)
 		if err3_1 != nil {
-			e:= fmt.Sprintf("[ReadData err3_1: %v]", err.Error())
+			e := fmt.Sprintf("[ReadData err3_1: %v]", err.Error())
 			err = errors.New(e)
 			return
 		}
-		address, err3_2 := ReadData(0, 5497, 160);
+		address, err3_2 := ReadData(0, 5497, 160)
 		if err3_2 != nil {
-			e:= fmt.Sprintf("[ReadData err3_2: %v]", err.Error())
+			e := fmt.Sprintf("[ReadData err3_2: %v]", err.Error())
 			err = errors.New(e)
 			return
 		}
-		image, err3_3 := ReadData(0, 377, 5120);
+		image, err3_3 := ReadData(0, 377, 5120)
 		if err3_3 != nil {
-			e:= fmt.Sprintf("[ReadData err3_3: %v]", err.Error())
+			e := fmt.Sprintf("[ReadData err3_3: %v]", err.Error())
 			err = errors.New(e)
 			return
 		}
 
 		smartcard = CreateSmartCard(cardversion, data, address, image)
-		
+
 	} else if cardversion == "0002" {
-		data, err2_1 := ReadData(1, 0, 377);
+		data, err2_1 := ReadData(1, 0, 377)
 		if err2_1 != nil {
-			e:= fmt.Sprintf("[ReadData err2_1: %v]", err.Error())
+			e := fmt.Sprintf("[ReadData err2_1: %v]", err.Error())
 			err = errors.New(e)
 			return
 		}
-		address, err2_2 := ReadData(0, 4, 150);
+		address, err2_2 := ReadData(0, 4, 150)
 		if err2_2 != nil {
-			e:= fmt.Sprintf("[ReadData err2_2: %v]", err.Error())
+			e := fmt.Sprintf("[ReadData err2_2: %v]", err.Error())
 			err = errors.New(e)
 			return
 		}
-		image, err2_3 := ReadData(1, 377, 5120);
+		image, err2_3 := ReadData(1, 377, 5120)
 		if err2_3 != nil {
-			e:= fmt.Sprintf("[ReadData err2_3: %v]", err.Error())
+			e := fmt.Sprintf("[ReadData err2_3: %v]", err.Error())
 			err = errors.New(e)
 			return
 		}
 
 		smartcard = CreateSmartCard(cardversion, data, address, image)
 	} else {
-		e:= fmt.Sprintf("[ReadCardData error: card version %v is not supported]", cardversion)
+		e := fmt.Sprintf("[ReadCardData error: card version %v is not supported]", cardversion)
 		err = errors.New(e)
 		return
 	}
@@ -565,7 +565,7 @@ func GetEnvelopeByVerifyPin(random string) (envelop string, err error) {
 		fmt.Printf("SelectApplet error: %#v\n", err.Error())
 		return
 	}
-	
+
 	adm_version, laser_number, err := GetInfoADM()
 	if err != nil {
 		fmt.Printf("GetInfoADM error: %#v\n", err.Error())
@@ -583,13 +583,13 @@ func GetEnvelopeByVerifyPin(random string) (envelop string, err error) {
 
 	// GetMatchStatus
 	in_buf := []byte(random)
-	out_buf, match_stt, err := GetMatchStatus( in_buf )
+	out_buf, match_stt, err := GetMatchStatus(in_buf)
 	if err != nil {
 		fmt.Printf("GetMatchStatus error: %v\n", err.Error())
 		return
 	}
 	if match_stt != 1 {
-		e := fmt.Sprintf("[GetMatchStatus: status=%v, miss match]",match_stt)
+		e := fmt.Sprintf("[GetMatchStatus: status=%v, miss match]", match_stt)
 		err = errors.New(e)
 		return
 	}
--- smartcard.go
+++ smartcard.go
@@ -2,15 +2,15 @@ package scapi
 
 import (
 	"fmt"
-	"strings"
 	"golang.org/x/text/encoding/charmap"
+	"strings"
 )
 
 type SmartCard struct {
 	m_card_version string
-    m_data []byte
-    m_address []byte
-    m_image []byte
+	m_data         []byte
+	m_address      []byte
+	m_image        []byte
 }
 
 func CreateSmartCard(card_version string, data []byte, address []byte, image []byte) *SmartCard {
@@ -35,16 +35,16 @@ func decodeWindows874(input string) string {
 }
 
 func (sc *SmartCard) GetPID() string {
-	if (sc.m_card_version == "0003" || sc.m_card_version == "0002") {
-		return string(sc.m_data[4:4+13])
+	if sc.m_card_version == "0003" || sc.m_card_version == "0002" {
+		return string(sc.m_data[4 : 4+13])
 	} else {
 		return ""
 	}
 }
 
 func (sc *SmartCard) GetFullName() string {
-	if (sc.m_card_version == "0003" || sc.m_card_version == "0002") {
-		fullname := string(sc.m_data[17:17+100])
+	if sc.m_card_version == "0003" || sc.m_card_version == "0002" {
+		fullname := string(sc.m_data[17 : 17+100])
 		f := decodeWindows874(fullname)
 		return strings.Trim(f, " ")
 	} else {
@@ -52,7 +52,7 @@ func (sc *SmartCard) GetFullName() string {
 	}
 }
 
-func (sc *SmartCard) GetTitle() (output string) { 
+func (sc *SmartCard) GetTitle() (output string) {
 	output = ""
 
 	fullname := sc.GetFullName()
@@ -101,8 +101,8 @@ func (sc *SmartCard) GetLastName() (output string) {
 }
 
 func (sc *SmartCard) GetFullName_En() string {
-	if (sc.m_card_version == "0003" || sc.m_card_version == "0002") {
-		fullname := string(sc.m_data[117:117+100])
+	if sc.m_card_version == "0003" || sc.m_card_version == "0002" {
+		fullname := string(sc.m_data[117 : 117+100])
 		f := decodeWindows874(fullname)
 		return strings.Trim(f, " ")
 	} else {
@@ -110,7 +110,7 @@ func (sc *SmartCard) GetFullName_En() string {
 	}
 }
 
-func (sc *SmartCard) GetTitle_En() (output string) { 
+func (sc *SmartCard) GetTitle_En() (output string) {
 	output = ""
 
 	fullname := sc.GetFullName_En()
@@ -158,9 +158,9 @@ func (sc *SmartCard) GetLastName_En() (output string) {
 	return
 }
 
-func (sc *SmartCard) GetBirthDate() string {	
-	if (sc.m_card_version == "0003" || sc.m_card_version == "0002") {
-		birthdate := string(sc.m_data[217:217+8])
+func (sc *SmartCard) GetBirthDate() string {
+	if sc.m_card_version == "0003" || sc.m_card_version == "0002" {
+		birthdate := string(sc.m_data[217 : 217+8])
 		return birthdate
 	} else {
 		return ""
@@ -168,8 +168,8 @@ func (sc *SmartCard) GetBirthDate() string {
 }
 
 func (sc *SmartCard) GetGender() string {
-	if (sc.m_card_version == "0003" || sc.m_card_version == "0002") {
-		gender := string(sc.m_data[225:225+1])
+	if sc.m_card_version == "0003" || sc.m_card_version == "0002" {
+		gender := string(sc.m_data[225 : 225+1])
 		return gender
 	} else {
 		return ""
@@ -178,8 +178,8 @@ func (sc *SmartCard) GetGender() string {
 
 // หมายเลขคำร้อง
 func (sc *SmartCard) GetRequestID() string {
-	if (sc.m_card_version == "0003" || sc.m_card_version == "0002") {
-		requestid := string(sc.m_data[226:226+20])
+	if sc.m_card_version == "0003" || sc.m_card_version == "0002" {
+		requestid := string(sc.m_data[226 : 226+20])
 		return requestid
 	} else {
 		return ""
@@ -188,8 +188,8 @@ func (sc *SmartCard) GetRequestID() string {
 
 // BP1NO
 func (sc *SmartCard) GetBP1NO() string {
-	if (sc.m_card_version == "0003" || sc.m_card_version == "0002") {
-		requestid := string(sc.m_data[226:226+11])
+	if sc.m_card_version == "0003" || sc.m_card_version == "0002" {
+		requestid := string(sc.m_data[226 : 226+11])
 		return requestid
 	} else {
 		return ""
@@ -198,8 +198,8 @@ func (sc *SmartCard) GetBP1NO() string {
 
 // สถานที่ออกบัตร
 func (sc *SmartCard) GetIssueLocation() string {
-	if (sc.m_card_version == "0003" || sc.m_card_version == "0002") {
-		issuelocation := string(sc.m_data[246:246+100])
+	if sc.m_card_version == "0003" || sc.m_card_version == "0002" {
+		issuelocation := string(sc.m_data[246 : 246+100])
 		issuelocation = decodeWindows874(issuelocation)
 		return strings.Trim(issuelocation, " ")
 	} else {
@@ -209,8 +209,8 @@ func (sc *SmartCard) GetIssueLocation() string {
 
 // ผู้ออกบัตร
 func (sc *SmartCard) GetIssuePersonID() string {
-	if (sc.m_card_version == "0003" || sc.m_card_version == "0002") {
-		data := string(sc.m_data[346:346+13])
+	if sc.m_card_version == "0003" || sc.m_card_version == "0002" {
+		data := string(sc.m_data[346 : 346+13])
 		data = decodeWindows874(data)
 		return strings.Trim(data, " ")
 	} else {
@@ -219,8 +219,8 @@ func (sc *SmartCard) GetIssuePersonID() string {
 }
 
 func (sc *SmartCard) GetIssueDate() string {
-	if (sc.m_card_version == "0003" || sc.m_card_version == "0002") {
-		data := string(sc.m_data[359:359+8])
+	if sc.m_card_version == "0003" || sc.m_card_version == "0002" {
+		data := string(sc.m_data[359 : 359+8])
 		data = decodeWindows874(data)
 		return strings.Trim(data, " ")
 	} else {
@@ -229,8 +229,8 @@ func (sc *SmartCard) GetIssueDate() string {
 }
 
 func (sc *SmartCard) GetExpireDate() string {
-	if (sc.m_card_version == "0003" || sc.m_card_version == "0002") {
-		data := string(sc.m_data[367:367+8])
+	if sc.m_card_version == "0003" || sc.m_card_version == "0002" {
+		data := string(sc.m_data[367 : 367+8])
 		data = decodeWindows874(data)
 		return strings.Trim(data, " ")
 	} else {
@@ -240,8 +240,8 @@ func (sc *SmartCard) GetExpireDate() string {
 
 func (sc *SmartCard) GetCardType() string {
 	// Output: 01 = บัตรประชาชน
-	if (sc.m_card_version == "0003" || sc.m_card_version == "0002") {
-		data := string(sc.m_data[375:375+2])
+	if sc.m_card_version == "0003" || sc.m_card_version == "0002" {
+		data := string(sc.m_data[375 : 375+2])
 		data = decodeWindows874(data)
 		return strings.Trim(data, " ")
 	} else {
@@ -250,14 +250,14 @@ func (sc *SmartCard) GetCardType() string {
 }
 
 func (sc *SmartCard) GetAddress() string {
-	if (sc.m_card_version == "0003") {
+	if sc.m_card_version == "0003" {
 		data := string(sc.m_address[0:160])
 		data = decodeWindows874(data)
 		return strings.Trim(data, " ")
-	}else if (sc.m_card_version == "0002") {
-			data := string(sc.m_address[0:150])
-			data = decodeWindows874(data)
-			return strings.Trim(data, " ")
+	} else if sc.m_card_version == "0002" {
+		data := string(sc.m_address[0:150])
+		data = decodeWindows874(data)
+		return strings.Trim(data, " ")
 	} else {
 		return ""
 	}
